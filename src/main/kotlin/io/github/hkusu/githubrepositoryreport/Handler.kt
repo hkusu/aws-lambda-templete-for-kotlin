3dc8123d96a045743daef9de53fada7cbce64dd9
Hiroyuki Kusu <kusu0806@gmail.com>
2020-05-15 16:34:13 +0900

    Update Handler.kt

--- src/main/kotlin/io/github/hkusu/githubrepositoryreport/Handler.kt
+++ src/main/kotlin/io/github/hkusu/githubrepositoryreport/Handler.kt
@@ -17,6 +17,7 @@ data class Input(
     var hoge: String? = null // varかつデフォルト値の指定が必要
 )
 
+// API Gateway の Lambda プロキシ統合の場合のreturn
 data class Output(
     val statusCode: Int,
     val body: String
